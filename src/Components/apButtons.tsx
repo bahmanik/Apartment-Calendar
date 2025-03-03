59249315ca101f7d57bb9d8229ee293aae0a7433
bahmanik <bahmaniali350@gmail.com>
2025-03-03 03:50:42 +0330

    desktop layout

--- src/Components/apButtons.tsx
+++ src/Components/apButtons.tsx
@@ -3,16 +3,7 @@ import overView from '../Assets/overview.svg';
 export const ApButtons = ({ ApName, onAp, onOv, onOpenFuzzy }: { ApName: { [key: string]: string }, onAp: (arg: string) => void, onOv: () => void, onOpenFuzzy: () => void }) => {
   return (
     <div className="buttonGroup">
-      <div id="SObutton">
-        <button id="search" onClick={onOpenFuzzy}>
-          <img src={search} className="buttonSVG" alt="Search" />
-        </button>
-        <button id="overView" onClick={onOv}>
-          <img src={overView} className="buttonSVG" alt="overView" />
-        </button>
-      </div>
-
-      <div className="apButton">
+      <div id="apButton">
         {
           Object.keys(ApName).map(e => <button
             className={e}
@@ -23,6 +14,15 @@ export const ApButtons = ({ ApName, onAp, onOv, onOpenFuzzy }: { ApName: { [key:
           </button>)
         }
       </div>
+      <div id="SObutton">
+        <button id="search" onClick={onOpenFuzzy}>
+          <img src={search} className="buttonSVG" alt="Search" />
+        </button>
+        <button id="overView" onClick={onOv}>
+          <img src={overView} className="buttonSVG" alt="overView" />
+        </button>
+      </div>
+
     </div>
   )
 }
